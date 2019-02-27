372f1276abe7184eab2004e34a4eec50705151cd
Aaron Kling <webgeek1234@gmail.com>
2019-02-27 11:56:26 -0600

    Add twrp support
    Change-Id: Ib8e0c17c28d5c41f6e274eef27337eb29c85e0c4

---
+++ twrp.mk
@@ -0,0 +1,5 @@
+TW_INCLUDE_CRYPTO           := true
+TW_NO_LEGACY_PROPS          := true
+TW_USE_TOOLBOX              := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_HAS_FORCED_RECOVERY      := true
