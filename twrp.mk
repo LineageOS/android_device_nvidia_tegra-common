606c47ae3bba1cbf101e1408fd96c66c7a568432
Aaron Kling <webgeek1234@gmail.com>
2019-02-27 11:56:26 -0600

    Add twrp support
    Change-Id: Ib8e0c17c28d5c41f6e274eef27337eb29c85e0c4

---
+++ twrp.mk
@@ -0,0 +1,7 @@
+BOARD_HAS_NO_REAL_SDCARD    := true
+RECOVERY_SDCARD_ON_DATA     := true
+TW_INCLUDE_CRYPTO           := true
+TW_NO_LEGACY_PROPS          := true
+TW_USE_TOOLBOX              := true
+TW_EXCLUDE_SUPERSU          := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
