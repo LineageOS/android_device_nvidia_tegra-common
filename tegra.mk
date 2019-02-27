f50b1280b89361feef437b945cee8dab4d445aaa
Aaron Kling <webgeek1234@gmail.com>
2019-02-27 12:22:14 -0600

    Add generic healthd hal service
    No known tegra devices need special healthd handling
    
    Change-Id: I3bb5697da01c9a51c37cb33a2008f7c1f6cf7e46

--- tegra.mk
+++ tegra.mk
@@ -41,3 +41,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
