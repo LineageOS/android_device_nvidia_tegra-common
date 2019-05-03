6b950008ab4bed483d959310ae9ac30f62b201cb
Aaron Kling <webgeek1234@gmail.com>
2019-05-03 15:17:07 -0500

    Add sepolicy for nvidia source built hidl hals
    Change-Id: I10f366cab1a46673566e6f3c5014859f6e77ef93

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -69,7 +69,8 @@ endif
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += \
-    device/nvidia/tegra-common/sepolicy/upstream
+    device/nvidia/tegra-common/sepolicy/upstream \
+    device/nvidia/tegra-common/sepolicy/lineage
 
 # TWRP Support
 ifeq ($(WITH_TWRP),true)
