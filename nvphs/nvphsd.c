18f6ea555e58c00f473fea66b44093053d64f465
Aaron Kling <webgeek1234@gmail.com>
2019-05-06 09:01:38 -0500

    Add common nvphs files
    Change-Id: I3c3de4450f67cda416baea595fba735b996c3be4

---
+++ nvphs/nvphsd.c
@@ -0,0 +1,20 @@
+#include <dlfcn.h>
+#include <unistd.h>
+
+int (*phsmain)();
+
+int main() {
+    void* dl = dlopen("libnvphsd.so", RTLD_NOW | RTLD_GLOBAL);
+    if (!dl) {
+        printf("Failed to load library: %s\n", dlerror());
+        return 1;
+    }
+    
+    phsmain = dlsym(dl, "phsmain");
+    if (!phsmain) {
+        printf("Failed to find phsmain: %s\n", dlerror());
+        return 1;
+    }
+    
+    phsmain();
+}
