5ba7e5abbab2c7e422cfe31d31be425150269349
Aaron Kling <webgeek1234@gmail.com>
2019-08-09 10:54:00 -0500

    Add support for drm hwcomposer
    Change-Id: Ic8026e6802a322c0694cb5910716c1448966467e

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -14,6 +14,14 @@
 # limitations under the License.
 #
 
+# Graphics
+ifeq ($(TARGET_TEGRA_GPU),drm)
+BOARD_GPU_DRIVERS         ?= nouveau
+BOARD_USES_DRM_HWCOMPOSER := true
+DEVICE_MANIFEST_FILE      += device/nvidia/tegra-common/manifests/drm.xml
+TARGET_USES_HWC2          := true
+endif
+
 # Health
 ifeq ($(TARGET_TEGRA_HEALTH),aosp)
 DEVICE_MANIFEST_FILE += device/nvidia/tegra-common/manifests/health.xml
