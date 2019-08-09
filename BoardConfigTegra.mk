d6b84a6f61eec6aafda3044a3605ee23e3c0226b
Aaron Kling <webgeek1234@gmail.com>
2019-08-09 12:01:00 -0500

    Add support for swiftshader
    Change-Id: Ib9cf025ca5762f2aa8b1dc1e071cbedf44bb7625

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -20,6 +20,8 @@ BOARD_GPU_DRIVERS         ?= nouveau
 BOARD_USES_DRM_HWCOMPOSER := true
 DEVICE_MANIFEST_FILE      += device/nvidia/tegra-common/manifests/drm.xml
 TARGET_USES_HWC2          := true
+else ifeq ($(TARGET_TEGRA_GPU),swiftshader)
+TARGET_USES_HWC2          := true
 endif
 
 # Health
