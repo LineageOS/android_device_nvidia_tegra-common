6aac08bf40e435b2bf46a1a9e6040c85bca453cb
Aaron Kling <webgeek1234@gmail.com>
2020-01-25 10:02:48 -0600

    Add support for software OMX codecs
    Change-Id: I2631e686f48c036c49ee81538ef098ec9157f17c

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -33,6 +33,11 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := system/libhidl/vintfdata/manifest_healthd_excl
 DEVICE_MANIFEST_FILE += device/nvidia/tegra-common/manifests/health.xml
 endif
 
+# Omx
+ifeq ($(TARGET_TEGRA_OMX),software)
+DEVICE_MANIFEST_FILE += device/nvidia/tegra-common/manifests/omx.xml
+endif
+
 # Power
 ifeq ($(TARGET_TEGRA_POWER),lineage)
 include hardware/nvidia/power/BoardPower.mk
