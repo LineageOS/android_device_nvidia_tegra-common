1d1f697a1b2a288f6b123f39851c6748d3a1f697
Martin Liu <liumartin@google.com>
2020-03-10 17:05:58 +0800

    Remove lmkd properties to enable new lmkd kill strategy
    With new strategy default values for lmkd properties should work
    and no property overrides are necessary.
    
    Bug: 143857475
    Test: lmkd_unit_test
    Change-Id: I52c8c93ad5adb5e741790939e381b7c0adae4261

--- properties.mk
+++ properties.mk
@@ -18,18 +18,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.profile.sap.server.enabled?=true
 endif
 
-# LMKd
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true
-
 # Graphics
 ifeq ($(TARGET_TEGRA_GPU),drm)
 PRODUCT_PROPERTY_OVERRIDES += \
