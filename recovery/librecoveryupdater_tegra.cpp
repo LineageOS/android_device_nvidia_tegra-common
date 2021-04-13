5ef2080357b46d6e7a4cb2f2216f34001b866cf5
Aaron Kling <webgeek1234@gmail.com>
2021-04-12 20:33:29 -0500

    recovery updater: Add function to parse cboot version
    The l4t bootloader does not pass it's version via kernel params.
    This searches the partition for a given version.
    
    Change-Id: Ic43b648e7e1884f9af74fca1412b750a1b16ac10

--- recovery/librecoveryupdater_tegra.cpp
+++ recovery/librecoveryupdater_tegra.cpp
@@ -17,6 +17,7 @@
 #include "edify/expr.h"
 #include "otautil/error_code.h"
 
+#include <algorithm>
 #include <fstream>
 
 #define DTSFN_PATH "/proc/device-tree/nvidia,dtsfilename"
@@ -51,6 +52,48 @@ Value* GetDTBName(const char* name, State* state,
                       name, DTSFN_PATH);
 }
 
+// The L4T bootloader does not pass any android props, so the only way to check
+// the bootloader version is to grep it directly from the partition.
+#define CBOOT_PATH "/dev/block/by-name/EBT"
+
+Value* CheckCbootVersion(const char* name, State* state,
+                         const std::vector<std::unique_ptr<Expr>>& argv) {
+    size_t argc = argv.size();
+    if (argc != 1) {
+        return ErrorAbort(state, kArgsParsingFailure,
+                          "%s() expects 1 args, got %zu", name, argc);
+    }
+
+    std::vector<std::string> args;
+    if (!ReadArgs(state, argv, &args)) {
+        return ErrorAbort(state, kArgsParsingFailure,
+                          "%s: could not parse the arguments.", name);
+    }
+
+    const std::string& bl_ver = args[0];
+
+    std::ifstream file(CBOOT_PATH, std::ios::binary);
+    if (!file.is_open()) {
+        return ErrorAbort(state, kFileOpenFailure, "%s() could not open %s",
+                          name, CBOOT_PATH);
+    }
+
+    file.seekg(0, std::ios::end);
+    auto size = file.tellg();
+    file.seekg(0);
+    std::vector<char> cboot_bin(size, '\0');
+    file.read(cboot_bin.data(), size);
+
+    if (std::search(cboot_bin.begin(), cboot_bin.end(), bl_ver.begin(),
+	            bl_ver.end()) !=
+        cboot_bin.end()) {
+        return StringValue("t");
+    }
+
+    return StringValue("");
+}
+
 void Register_librecoveryupdater_tegra() {
     RegisterFunction("tegra_get_dtbname", GetDTBName);
+    RegisterFunction("tegra_check_cboot_version", CheckCbootVersion);
 }
