41700ad11b042fc3323ab115e057b62cd55a83c1
Aaron Kling <webgeek1234@gmail.com>
2021-12-14 00:44:33 -0600

    Update init files for rel-shield-r
    Change-Id: I5d1bf0867b923bf1e01ef2adde2106f8317d5c2a

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -9,14 +9,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE               := init.data_bin.rc
-LOCAL_MODULE_CLASS         := ETC
-LOCAL_SRC_FILES            := init.data_bin.rc
-LOCAL_VENDOR_MODULE        := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE               := init.gps.rc
 LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.gps.rc
