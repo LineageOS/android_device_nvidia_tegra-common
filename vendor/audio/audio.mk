28884efe8d3561ff896b8c4e379fc8c53c16dd99
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-17 17:33:11 -0500

    vendor: Drop dead package references
    * Neither of these are necessary either.
    
    Change-Id: I3f43de979147f4b07b45db73a104a40b0db740e1

--- vendor/audio/audio.mk
+++ vendor/audio/audio.mk
@@ -14,13 +14,11 @@
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio@6.0 \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.common@6.0 \
     android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
