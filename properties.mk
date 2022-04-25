fa4b82e83b9153aef938c6e4b398e8cb5c71623b
Sal Savage <salsavage@google.com>
2022-04-25 16:43:16 -0700

    properties: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Change-Id: I24fd57bacbf6786a26f7079e7a6e9857dbe170d1

--- properties.mk
+++ properties.mk
@@ -1,6 +1,7 @@
 # Bluetooth
 ifneq ($(TARGET_TEGRA_BT),)
 PRODUCT_PROPERTY_OVERRIDES += \
+    bluetooth.device.class_of_device=90,2,12 \
     bluetooth.profile.asha.central.enabled?=true \
     bluetooth.profile.a2dp.source.enabled?=true \
     bluetooth.profile.avrcp.target.enabled?=true \
