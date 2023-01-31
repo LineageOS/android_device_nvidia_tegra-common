b5697d90a8132a54b0bf37329bf949db80273c40
Aaron Kling <webgeek1234@gmail.com>
2023-01-31 01:36:40 -0600

    vendor: BMP: Explicitly set path to bup
    Instead of assuming it's set correctly by the device
    
    Change-Id: I0f520e57b1966db504d04f8741a7f8bb26313fbb

--- vendor/BMP/Android.mk
+++ vendor/BMP/Android.mk
@@ -15,6 +15,8 @@
 LOCAL_PATH := $(call my-dir)
 COMMON_BMP_PATH := $(BUILD_TOP)/vendor/nvidia/common/BMP
 
+BUP_PATH := $(BUILD_TOP)/vendor/nvidia/common/tegraflash
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := bmp.blob
 LOCAL_MODULE_CLASS := ETC
@@ -24,6 +26,6 @@ _bmp_blob_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(
 _bmp_blob := $(_bmp_blob_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 
 $(_bmp_blob):
-	OUT=$(dir $@) TOP=$(BUILD_TOP) python2 $(TEGRAFLASH_PATH)/BUP_generator.py -t bmp -e "$(COMMON_BMP_PATH)/nvidia1080.bmp nvidia 1080; $(COMMON_BMP_PATH)/verity_orange_continue_1080.bmp verity_orange_continue 1080; $(COMMON_BMP_PATH)/verity_orange_pause_1080.bmp verity_orange_pause 1080; $(COMMON_BMP_PATH)/verity_red_continue_1080.bmp verity_red_continue 1080; $(COMMON_BMP_PATH)/verity_red_pause_1080.bmp verity_red_pause 1080; $(COMMON_BMP_PATH)/verity_yellow_continue_1080.bmp verity_yellow_continue 1080; $(COMMON_BMP_PATH)/verity_yellow_pause_1080.bmp verity_yellow_pause 1080"
+	OUT=$(dir $@) TOP=$(BUILD_TOP) python2 $(BUP_PATH)/BUP_generator.py -t bmp -e "$(COMMON_BMP_PATH)/nvidia1080.bmp nvidia 1080; $(COMMON_BMP_PATH)/verity_orange_continue_1080.bmp verity_orange_continue 1080; $(COMMON_BMP_PATH)/verity_orange_pause_1080.bmp verity_orange_pause 1080; $(COMMON_BMP_PATH)/verity_red_continue_1080.bmp verity_red_continue 1080; $(COMMON_BMP_PATH)/verity_red_pause_1080.bmp verity_red_pause 1080; $(COMMON_BMP_PATH)/verity_yellow_continue_1080.bmp verity_yellow_continue 1080; $(COMMON_BMP_PATH)/verity_yellow_pause_1080.bmp verity_yellow_pause 1080"
 
 include $(BUILD_SYSTEM)/base_rules.mk
