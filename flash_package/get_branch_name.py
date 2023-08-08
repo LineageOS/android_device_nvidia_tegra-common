1c03413ce08b04a139c1a06fbc86560d7d7bce99
Aaron Kling <webgeek1234@gmail.com>
2023-08-08 12:56:59 -0500

    Add edk2 prebuilts
    Adding prebuilts for use with t194+ as the source repos will not be
    forked yet.
    
    All prebuilts built from repos at
    https://gitlab.incom.co/CM-Shield
    
    edk2@fde1097
    edk2-non-osi@61662e8
    edk2-platform@3c3b1168
    edk2-nvidia@fc6faa5
    edk2-nvidia-non-osi@b1a1a1c
    edk2-nvidia-lineage@237179f
    
    Change-Id: I2b363aee40f560b492b35c2879b9d000116aea28

---
+++ flash_package/get_branch_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+
+import os
+import sys
+from xml.dom.minidom import parse
+
+try:
+    top_dir = os.environ['TOP']
+except Exception as e:
+    try:
+        top_dir = os.environ['BUILD_TOP']
+    except Exception as e:
+        print("unknown")
+        sys.exit()
+
+manifest_file = top_dir + '/.repo/manifests/default.xml'
+xml_tree = parse(manifest_file)
+collection = xml_tree.documentElement
+projects = collection.getElementsByTagName("default")
+
+for prj in projects:
+    if prj.getAttribute("remote") == "github":
+        ver = prj.getAttribute("revision").split('-')[1].split('.')
+        print("%s.%s" % (ver[0].zfill(2), ver[1].zfill(2)))
+        break
