6275547948d9963ae9ba71119cc2b84ddab1a3a6
Nolen Johnson <johnsonnolen@gmail.com>
2023-08-23 01:15:45 -0400

    tegra: Properly exclude AudioFX
    * Lineage changed this to use product makefiles, not Android.mk's.
    
    Refernce: https://review.lineageos.org/q/topic:%22eleven-audiofx-exclude%22
    Change-Id: Ia0103ec2fde2ed5ad6958cd670f05df3a330ce7d

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -15,7 +15,14 @@
 #
 
 # Audio
-TARGET_EXCLUDES_AUDIOFX := true
+ifneq ($(TARGET_TEGRA_AUDIO),)
+ifeq ($(filter audio, $(TARGET_TEGRA_DOLBY)),)
+DEVICE_MANIFEST_FILE += device/nvidia/tegra-common/manifests/audio.xml
+endif
+ifeq ($(TARGET_TEGRA_AUDIO),tinyhal)
+BOARD_USES_TINYHAL_AUDIO := true
+endif
+endif
 
 # Bluetooth
 ifneq ($(TARGET_TEGRA_BT),)
