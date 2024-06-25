bbdc24fd4197df772b3ae22c7bfb87dc0e36b13b
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- tegra.mk
+++ tegra.mk
@@ -134,7 +134,9 @@ endif
 endif
 
 # Boot Control
-ifneq ($(TARGET_TEGRA_BOOTCTRL),)
+ifeq ($(TARGET_TEGRA_BOOTCTRL),)
+AB_OTA_UPDATER := false
+else
 AB_OTA_UPDATER := true
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
@@ -149,6 +151,7 @@ else ifeq ($(TARGET_TEGRA_BOOTCTRL),efi)
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl.nvidia-efi \
     android.hardware.boot@1.0-impl.nvidia-efi.recovery
+endif
 endif
 endif
 
