df4a9772464a61f40128b0d6ee4e5d78b45f354f
Aaron Kling <webgeek1234@gmail.com>
2024-06-27 01:27:36 -0500

    swiftshader: Look for angle on system
    On Android 14 QPR2, angle was installed to vendor and a property needed
    set for the frameworks to find it. On QPR3, angle is installed to system
    by default and the property needs removed for the frameworks to find it.
    
    Change-Id: Ic39862736c139dbcd3c00efb537bc02323e67518

--- tegra.mk
+++ tegra.mk
@@ -204,10 +204,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     hwcomposer.drm_minigbm \
     gralloc.minigbm \
-    vulkan.pastel \
-    libEGL_angle \
-    libGLESv1_CM_angle \
-    libGLESv2_angle
+    vulkan.pastel
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
