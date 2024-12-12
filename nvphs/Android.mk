6d52f38574330a14a4928b2bdc56599009aa7312
Thomas Makin <halorocker89@gmail.com>
2024-12-12 16:56:17 +0000

    power: stub out nvphs if using oss power stack
    Change-Id: I6d49327102842cde0f52c78fbdf30969d9e4ed84

--- nvphs/Android.mk
+++ nvphs/Android.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-ifneq ($(TARGET_TEGRA_PHS),)
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_TEGRA_PHS),)
 include $(CLEAR_VARS)
 LOCAL_MODULE               := nvphsd
 LOCAL_SRC_FILES            := nvphsd.c
@@ -24,4 +24,14 @@ LOCAL_SHARED_LIBRARIES     := libc libdl libnvphsd
 LOCAL_MODULE_CLASS         := EXECUTABLES
 LOCAL_VENDOR_MODULE        := true
 include $(BUILD_EXECUTABLE)
+else
+include $(CLEAR_VARS)
+LOCAL_MODULE               := libnvphs
+LOCAL_MODULE_CLASS         := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS          := optional
+LOCAL_MODULE_SUFFIX        := .so
+LOCAL_VENDOR_MODULE        := true
+LOCAL_SRC_FILES            := nvphs_stub.c
+LOCAL_CFLAGS               := -Wall -Wno-unused-parameter -Werror
+include $(BUILD_SHARED_LIBRARY)
 endif
