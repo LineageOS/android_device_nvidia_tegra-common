f0f9a0d6e3a1421767609e788b2233952992d400
Thomas Makin <halorocker89@gmail.com>
2024-12-13 23:46:33 +0000

    tegra: introduce TARGET_TEGRA_CPL and stub nvcpl
    nvcpl should not build by default if not using standard power stack.
    Instead, introduce a stub for libnvcpl_vendor.
    
    Change-Id: I2bebaf9bebfbac783fd7eed945745cd55d358969

--- vendor/rel-shield-r/nvcpl/Android.mk
+++ vendor/rel-shield-r/nvcpl/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_TEGRA_DEFAULT_BRANCH),rel-shield-r)
+ifeq ($(TARGET_TEGRA_CPL),rel-shield-r)
 LOCAL_PATH := $(call my-dir)
 COMMON_NVCPL_PATH := ../../../../../../vendor/nvidia/common/rel-shield-r/nvcpl
 
@@ -65,6 +65,7 @@ LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_OWNER         := nvidia
 LOCAL_VENDOR_MODULE        := true
 include $(BUILD_NVIDIA_COMMON_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE               := libnvcontrol_jni
@@ -76,4 +77,3 @@ LOCAL_MODULE_CLASS         := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS          := optional
 LOCAL_MODULE_OWNER         := nvidia
 include $(BUILD_NVIDIA_COMMON_PREBUILT)
-endif
