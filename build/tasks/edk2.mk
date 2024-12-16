5aad29e32d628ad3d8939b85b1bd58c731ac989c
Aaron Kling <webgeek1234@gmail.com>
2024-12-15 22:23:51 -0600

    Move bootloader build rules to build tasks
    Android.mk is being deprecated and these rules kinda abused the concept
    anyways.
    
    Change-Id: I511d2594ed3b81efb6208994658a821777d8653e

--- build/tasks/edk2.mk
+++ build/tasks/edk2.mk
@@ -14,8 +14,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
+ifneq ($(TARGET_TEGRA_VERSION),)
 TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)
 
 BUILD_TOOLS_PATH   := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)
@@ -37,8 +36,11 @@ EDK2_TARGET        := JetsonAndroid
 
 TOOLCHAIN_VARS := CLANG_HOST_BIN=$(abspath $(dir $(KERNEL_MAKE_CMD)))/ CC=$(TARGET_KERNEL_CLANG_PATH)/bin/clang CXX=$(TARGET_KERNEL_CLANG_PATH)/bin/clang++ AS=$(TARGET_KERNEL_CLANG_PATH)/bin/clang AR=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-ar LD=$(TARGET_KERNEL_CLANG_PATH)/bin/llvm-ld CLANG_BIN=$(TARGET_KERNEL_CLANG_PATH)/bin/ GCC_HOST_BIN=$(abspath $(dir $(KERNEL_MAKE_CMD)))/ GCC5_$(EDK2_ARCH)_PREFIX=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX) DTCPP_PREFIX=$(KERNEL_TOOLCHAIN)/$(KERNEL_TOOLCHAIN_PREFIX)
 
+# 1: Intermediates dir
+# 2: Target base filename
+# 3: Optional build paramters
 define edk2_rule
-	echo "got $1 - $2"
+$(1): $(DTC_HOST)
 	@mkdir -p $(dir $1)
 	@cp -R $(TIANOCORE_PATH)/edk2 $(dir $1)/
 	@cp -R $(TIANOCORE_PATH)/edk2-non-osi $(dir $1)/
@@ -56,38 +58,32 @@ define edk2_rule
 		export $(TOOLCHAIN_VARS) && \
 		source $(abspath $(dir $1))/edk2/edksetup.sh && \
 		$(KERNEL_MAKE_CMD) -C $(abspath $(dir $1))/edk2/BaseTools && \
-		PYTHONPATH=$$PYTHONPATH:$(abspath $(dir $1))/edk2-nvidia-lineage/Silicon/NVIDIA/scripts/Kconfiglib \
+		PYTHONPATH=$$$$PYTHONPATH:$(abspath $(dir $1))/edk2-nvidia-lineage/Silicon/NVIDIA/scripts/Kconfiglib \
 			KCONFIG_CONFIG=$(abspath $(dir $1))/edk2-nvidia-lineage/.config \
 			$(abspath $(dir $1))/edk2-nvidia-lineage/Silicon/NVIDIA/scripts/Kconfiglib/defconfig.py \
 			--kconfig $(abspath $(dir $1))/edk2-nvidia/Platform/NVIDIA/Kconfig \
 			$(abspath $(dir $1))/edk2-nvidia-lineage/Platform/NVIDIA/$(EDK2_TARGET)/$(EDK2_TARGET).defconfig && \
 		sed 's/"//g' $(abspath $(dir $1))/edk2-nvidia-lineage/.config > $(abspath $(dir $1))/edk2-nvidia-lineage/config.dsc.inc && \
-		sed -E -i 's/^CONFIG_([^=]*)=$$/# CONFIG_\1 is not set/' $(abspath $(dir $1))/edk2-nvidia-lineage/config.dsc.inc && \
+		sed -E -i 's/^CONFIG_([^=]*)=$$$$/# CONFIG_\1 is not set/' $(abspath $(dir $1))/edk2-nvidia-lineage/config.dsc.inc && \
 		$(abspath $(dir $1))/edk2/BaseTools/BinWrappers/PosixLike/build -q -a $(EDK2_ARCH) -t $(EDK2_TOOLCHAIN) \
 			-p $(abspath $(dir $1))/edk2-nvidia-lineage/Platform/NVIDIA/$(EDK2_TARGET)/$(EDK2_TARGET).dsc -b $(EDK2_BUILD_TYPE) \
-			$2 -DEDK2_PKCS7_INC=$(EDK2_PKCS7_INC) -DBUILD_PROJECT_TYPE=EDK2 -DBUILD_DATE_TIME="$(shell date '+%Y-%m-%dT%H:%M:%S+00:00')" \
+			$3 -DEDK2_PKCS7_INC=$(EDK2_PKCS7_INC) -DBUILD_PROJECT_TYPE=EDK2 -DBUILD_DATE_TIME="$(shell date '+%Y-%m-%dT%H:%M:%S+00:00')" \
 			-DBUILDID_STRING="$(shell BUILD_TOP=$(abspath $(TIANOCORE_PATH)/../..) python $(SCRIPTS_PATH)/get_branch_name.py)-$(shell git -C $(TIANOCORE_PATH)/edk2-nvidia-lineage rev-parse --short HEAD)"
+	@python $(dir $1)/edk2-nvidia/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py $(dir $1)/Build/$(EDK2_TARGET)/$(EDK2_BUILD_TYPE)_$(EDK2_TOOLCHAIN)/FV/UEFI_NS.Fv $1
+
+$(PRODUCT_OUT)/$(2).bin: $(1)
+	$(hide) cp $(1) $(PRODUCT_OUT)/$(2).bin
+
+.PHONY: $(2)
+$(2): $(PRODUCT_OUT)/$(2).bin
 endef
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE        := tianocore
-LOCAL_MODULE_SUFFIX := .bin
-LOCAL_MODULE_CLASS  := EXECUTABLES
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
-
-_tianocore_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_tianocore_bin := $(_tianocore_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-
-$(_tianocore_bin): $(DTC_HOST)
-	$(call edk2_rule,$@)
-	@python $(dir $@)/edk2-nvidia/Silicon/NVIDIA/edk2nv/FormatUefiBinary.py $(dir $@)/Build/$(EDK2_TARGET)/$(EDK2_BUILD_TYPE)_$(EDK2_TOOLCHAIN)/FV/UEFI_NS.Fv $@
-
-include $(BUILD_SYSTEM)/base_rules.mk
+$(eval $(call edk2_rule,$(call intermediates-dir-for,EXECUTABLES,tianocore)/tianocore.bin,tianocore))
 
 TIANOCORE_DTBO_TARGETS := \
     AndroidConfiguration.dtbo
 INSTALLED_TIANOCORE_DTBO_TARGETS := $(TIANOCORE_DTBO_TARGETS:%=$(PRODUCT_OUT)/%)
 $(INSTALLED_TIANOCORE_DTBO_TARGETS): $(PRODUCT_OUT)/tianocore.bin
 	echo -e ${CL_GRN}"Copying individual tianocore DTBOs"${CL_RST}
-	cp $(@F:%.dtbo=$(_tianocore_intermediates)/Build/$(EDK2_TARGET)/$(EDK2_BUILD_TYPE)_$(EDK2_TOOLCHAIN)/$(EDK2_ARCH)/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/%.dtb) $@
+	cp $(@F:%.dtbo=$(call intermediates-dir-for,EXECUTABLES,tianocore)/Build/$(EDK2_TARGET)/$(EDK2_BUILD_TYPE)_$(EDK2_TOOLCHAIN)/$(EDK2_ARCH)/Silicon/NVIDIA/Tegra/DeviceTree/DeviceTree/OUTPUT/%.dtb) $@
+endif
