8733d6bf414f7b189b688685f129b99b0d37690a
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 19:58:34 -0600

    shims: Drop nvos shim
    No users of this on current devices
    
    Change-Id: I870b5e70fbfdb7d8aa2044f277a1fbf08d510af8

--- shims/nvos_shim.cpp
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-extern "C" uint32_t NvOsGetConfigString(const char *name, char *value, uint32_t size);
-
-extern "C" uint32_t NvOsConfigGetState(int dummy1, const char *name, char *value, int size, int dummy2) {
-  return NvOsGetConfigString(name, value, size);
-}
