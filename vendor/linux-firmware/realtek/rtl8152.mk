e307493abfc0e62f3debd6a2c444d794f3b2166d
Aaron Kling <webgeek1234@gmail.com>
2025-03-21 16:04:16 -0500

    linux-firmware: Add rtl8152 from aosp
    This supercede's the rtl8153a-3 support. Android 15 QPR2 added a repo to
    track some linux-firmware support, and rtl8152 is part of the initial
    support. This conflicts with the rtl8153-a3 copy that was here before,
    so just use the aosp support. It does copy a few more firmware files,
    but the file size is trivial. Leaving the original makefile until all
    users have switched.
    
    Change-Id: Id56c0d5200524ec68a81a98abb5795272e05ba38

--- vendor/linux-firmware/realtek/rtl8152.mk
+++ vendor/linux-firmware/realtek/rtl8152.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2024-2025 The LineageOS Project
+# Copyright (C) 2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/nvidia/tegra-common/vendor/linux-firmware/realtek/rtl8152.mk
+PRODUCT_PACKAGES += \
+    linux_firmware_r8152
