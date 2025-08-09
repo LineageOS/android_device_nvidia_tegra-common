eaa3c80c3c67b15d1c09931499debea39d16ffd5
LuK1337 <priv.luk@gmail.com>
2025-08-09 11:09:53 +0200

    tegra-common: Remove vendor/lineage device framework matrix inclusion
    See: I78da6340f38b93fdc4c3694cb8f431f387d16c0d
    Change-Id: I0b9f527de50ca02082be9b92375559f4893b2688

--- BoardConfigTegra.mk
+++ BoardConfigTegra.mk
@@ -63,10 +63,6 @@ endif
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     device/nvidia/tegra-common/manifests/device_framework_matrix.xml
-ifneq ($(LINEAGE_BUILD),)
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/lineage/config/device_framework_matrix.xml
-endif
 
 # Keystore
 ifneq ($(TARGET_TEGRA_TOS),)
